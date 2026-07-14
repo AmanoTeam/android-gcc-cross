2791ff56770af07728ec2694406ec8aaedff274f
Kartatz <105828205+Kartatz@users.noreply.github.com>
2026-07-14 16:49:07 -0300

    Accept unified-syntax conditional suffix form in divided syntax

--- build.sh
+++ build.sh
@@ -386,6 +386,7 @@ if ! [ -f "${binutils_tarball}" ]; then
 	
 	patch --directory="${binutils_directory}" --strip='1' --input="${workdir}/submodules/obggcc/patches/0001-Add-relative-RPATHs-to-binutils-host-tools.patch"
 	patch --directory="${binutils_directory}" --strip='1' --input="${workdir}/submodules/obggcc/patches/0001-Don-t-warn-about-local-symbols-within-the-globals.patch"
+	patch --directory="${binutils_directory}" --strip='1' --input="${workdir}/patches/0001-ARM-Accept-unified-syntax-conditional-suffix-form-in-divided-syntax.patch"
 fi
 
 if ! [ -f "${gold_tarball}" ]; then
