5174fd421e5079bda8570890bbbcb4e8c572a40a
Kartatz <105828205+Kartatz@users.noreply.github.com>
2026-08-01 19:25:00 -0300

    Add --pack-dyn-relocs=android[+relr] support

--- build.sh
+++ build.sh
@@ -388,6 +388,7 @@ if ! [ -f "${binutils_tarball}" ]; then
 	patch --directory="${binutils_directory}" --strip='1' --input="${workdir}/submodules/obggcc/patches/0001-Don-t-warn-about-local-symbols-within-the-globals.patch"
 	patch --directory="${binutils_directory}" --strip='1' --input="${workdir}/submodules/obggcc/patches/0001-ARM-Accept-unified-syntax-conditional-suffix-form-in-divided-syntax.patch"
 	patch --directory="${binutils_directory}" --strip='1' --input="${workdir}/submodules/obggcc/patches/0001-ELF-Accept-.rodata-.tdata-and-.tbss-section-directives-like-LLVM.patch"
+	patch --directory="${binutils_directory}" --strip='1' --input="${workdir}/patches/0001-ld-Add-pack-dyn-relocs-android-relr-support.patch"
 fi
 
 if ! [ -f "${gold_tarball}" ]; then
