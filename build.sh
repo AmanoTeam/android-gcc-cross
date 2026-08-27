d07e22395ea7f83581ca1722383301a860ddf4a8
Kartatz <105828205+Kartatz@users.noreply.github.com>
2026-08-27 09:44:11 -0300

    Fix C++ modules support

--- build.sh
+++ build.sh
@@ -103,10 +103,12 @@ declare -ra targets=(
 	# 'mipsel-unknown-linux-android'
 	# 'mips64el-unknown-linux-android'
 	# 'armv5-unknown-linux-androideabi'
-	'x86_64-unknown-linux-android'
-	'i686-unknown-linux-android'
+	
+	# 'x86_64-unknown-linux-android'
+	# 'i686-unknown-linux-android'
 	'aarch64-unknown-linux-android'
-	'armv7-unknown-linux-androideabi'
+	# 'armv7-unknown-linux-androideabi'
+	
 	# 'riscv64-unknown-linux-android'
 )
 
