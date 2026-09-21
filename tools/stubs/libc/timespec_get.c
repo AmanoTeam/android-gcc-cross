77940a18c0784fcf1070176c64e8ba2c8a74f6f5
Kartatz <105828205+Kartatz@users.noreply.github.com>
2026-09-21 08:08:08 -0300

    Remove some stubs

--- tools/stubs/libc/timespec_get.c
+++
@@ -1,5 +0,0 @@
-#include <time.h>
-
-int timespec_get(struct timespec* __ts, int __base) {
-	return (clock_gettime(__base - 1, __ts) != -1) ? __base : 0;
-}
--- tools/stubs/libc/timespec_getres.c
+++
@@ -1,5 +0,0 @@
-#include <time.h>
-
-int timespec_getres(struct timespec* __ts, int __base) {
-	return (clock_getres(__base - 1, __ts) != -1) ? __base : 0;
-}
